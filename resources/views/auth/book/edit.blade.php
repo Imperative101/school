f0833f64b1db29ca7f991e39c993709c3d13704b
Imperative101 <71561063+Imperative101@users.noreply.github.com>
2021-09-07 15:29:12 +0200

    Initial commit

---
+++ resources/views/auth/book/edit.blade.php
@@ -0,0 +1,20 @@
+<form method="POST" action="{{route('book.update',[$book])}}">
+<div class="form-group">
+  <label>Title</label>
+  <input type="text" name="book_title"  class="form-control" value="{{$book->title}}">
+  <small class="form-text text-muted">Knygos pavadinimas.</small>
+  </div>
+
+       ISBN: <input type="text" name="book_isbn" value="{{$book->isbn}}">
+       Pages: <input type="text" name="book_pages" value="{{$book->pages}}">
+       About: <textarea name="book_about">{{$book->about}}</textarea>
+       <select name="author_id">
+           @foreach ($authors as $author)
+               <option value="{{$author->id}}" @if($author->id == $book->author_id) selected @endif>
+                   {{$author->name}} {{$author->surname}}
+               </option>
+           @endforeach
+</select>
+       @csrf
+       <button type="submit">EDIT</button>
+</form>
